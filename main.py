f71ab523316df72f2bf4aad6f3104798304d605e
MaritaBaro <crazysky700@gmail.com>
2022-09-05 16:10:54 +0300

    Refactoring.

--- main.py
+++ main.py
@@ -1,46 +1,36 @@
 
-from shapes_functions import get_circle_perimeter_area, get_square_perimeter_area, get_rect_perimeter_area_side, \
-    get_rect_perimeter_area_co
+from shapes_functions import get_circle_perimeter_area, get_square_perimeter_area, read_square, read_circle, read_rect, \
+    get_rect_perimetr_area
+
+figurs = {'square': {'number': 1, 'read': read_square, 'calculate': get_square_perimeter_area},
+          'rectangle': {'number': 2, 'read': read_rect, 'calculate': get_rect_perimetr_area},
+          'circle': {'number': 3, 'read': read_circle, 'calculate': get_circle_perimeter_area}}
 
 try:
 
     while True:
-        question = int(input('Choose the geometric shape and calculate its area and perimeter:\n'
-                             '- if it is a square, then press 1 \n'
-                             '- if it is a rectangle, then press 2 \n'
-                             '- if it is a circle, then press 3\n'
-                             'Number your shape is: '))
+        prompt = 'Choose the geometric shape and calculate its area and perimeter:\n'
+        for name, figur in figurs.items():
+            prompt += f'- if it is a {name}, then press {figur["number"]} \n'
+        prompt += f'Number your shape is: '
+        question = int(input(prompt))
+
         while True:
-            if question == 1:
-
-                side = int(input("Enter side of square: "))
-                output = get_square_perimeter_area(side)
-                print(output)
-                break
-
-            elif question == 2:
-                ab_list = input("Enter first side or BottomLeft of rectangle: ").split()
-                if len(ab_list) == 1:
-                    cd_list = input("Enter second side of rectangle: ").split()
-                    output = get_rect_perimeter_area_side(ab_list, cd_list)
+            figur_found = False
+            for figur in figurs.values():
+                if question == figur['number']:
+                    *date, = figur['read']()
+                    output = figur['calculate'](*date)
                     print(output)
+                    figur_found = True
                     break
-                else:
-                    cd_list = input("Enter TopRight of rectangle: ").split()
-                    output = get_rect_perimeter_area_co(ab_list, cd_list)
-                    print(output)
-                    break
-
-            elif question == 3:
-                radius = int(input("Enter Radius of circle: "))
-                output = get_circle_perimeter_area(radius)
-                print(output)
+            if figur_found:
                 break
             else:
                 print('Please, enter 1, 2 or 3.')
                 question = int(input('Number your shape is: '))
 
-        answer = input("Do you want to calculate another shape? y/n ")
+        answer = input("Do you want to calculate another shape? y/n  ")
         if answer == 'y':
             pass
         else:
--- shapes_functions.py
+++ shapes_functions.py
@@ -1,6 +1,33 @@
 import math
 
 
+def read_circle():
+    radius = int(input("Enter Radius of circle: "))
+    return radius,
+
+
+def read_square():
+    side = int(input("Enter side of square: "))
+    return side,
+
+
+def read_rect():
+    ab_list = input("Enter first side or BottomLeft of rectangle: ").split()
+    if len(ab_list) == 1:
+        cd_list = input("Enter second side of rectangle: ").split()
+    else:
+        cd_list = input("Enter TopRight of rectangle: ").split()
+    return ab_list, cd_list
+
+
+def get_rect_perimetr_area(ab_list,cd_list):
+    if len(ab_list) == 1:
+        output = get_rect_perimeter_area_side(ab_list, cd_list)
+    else:
+        output = get_rect_perimeter_area_co(ab_list, cd_list)
+    return output
+
+
 def get_circle_perimeter_area(radius):
     circle_perimeter = int(2 * math.pi * radius)
     circle_area = int(math.pi * radius**2)
